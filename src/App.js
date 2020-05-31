d0b0a3ff3a3874c9173c7c08953cc5f25ab0d035
Nvejqygroqxk <44037680+Nvejqygroqxk@users.noreply.github.com>
2020-05-31 21:08:52 +0200

    tests 12/12

--- src/App.js
+++ src/App.js
@@ -9,53 +9,50 @@ function App() {
   const randomIndex = useSelector(state => state.randomIndex);
   const dispatch = useDispatch();
   const possibleAnswers = [
-    'A good soldier obeys without question. A good officer commands without doubt.',
-    'Blessed is the mind too small for doubt.',
-    'To admit defeat is to blaspheme against the Emperor.',
-    'For those who seek perfection there can be no rest on this side of the grave.',
-    'The difference between heresy and treachery is ignorance.',
-    'Knowledge is power, guard it well.',
-    'An open mind is like a fortress with its gates unbarred and unguarded.',
-    'Innocence proves nothing.',
-    'Success is commemorated, Failure merely remembered.',
-    'Even a man who has nothing can still offer his life.',
-    'Only in death does duty end.',
-    'No man died in His service that died in vain.',
-    'Hope is the first step on the road to disappointment.',
-    'There is no such thing as innocence, only degrees of guilt.',
-    'Beginning reform is beginning revolution.',
-    'Educate men without faith and you but make them clever devils.',
-    'Success is measured in blood, yours or your enemy\'s.',
-    'The man who has nothing can still have faith.',
-    'Burn the heretic. Kill the mutant. Purge the unclean.',
-    'It is better to die for the Emperor than to live for yourself.',
-    'Fear denies faith.',
-    'Foolish are those who fear nothing, yet claim to know everything.',
-    'Brave are they who know everything yet fear nothing.',
-    'Happiness is a delusion of the weak.',
-    'All souls call out for salvation.',
-    'Life is the Emperor\'s currency, spend it well.',
-    'A suspicious mind is a healthy mind.',
-    'Cowards die in shame.',
-    'Faith without deeds is worthless.',
-    'True Happiness stems only from Duty.',
-    'The blood of martyrs is the seed of the Imperium.',
-    'Heresy grows from idleness.',
-    'There is only the Emperor, and he is our shield and protector.',
-    'Truth is Subjective.',
-    'Damnation is Eternal.',
-    'Know the Mutant, Kill the Mutant.',
-    'To Question is to doubt.',
-    'He who keeps silent consents.',
-    'Prayer cleanses the soul, Pain cleanses the body.',
-    'Death by thy Compass.',
-    'Zeal is its own Excuse.',
-    'Work earns Salvation.',
-    'Without him there is nothing.',
-    'Only the Emperor is all.',
-    'Hatred is the emperor\'s greatest gift to humanity.',
-    'Victory needs no explanation, defeat allows none.',
-    'A small mind is easily filled with faith.'
+    'Orkz is made fer\' two fings! Fightin\', and winnin\'!',
+    'Orkses is never defeated in battle. If we win we win, if we die we die fighting so it don\'t count. If we runs for it we don\'t die neither, cos we can come back for annuver go, see!',
+    'Get \'em boyz! Dakka dakka dakka! WAAAGH! THE ORKS! WAAAGH!',
+    'We don\'t fight fer food, or fer teef, or guns, or cos we\'s told to fight. We fight cos we woz born to fight. And win.',
+    'Humies is all weak scum that deserve ta get stomped. \'Cept for One-Eye Yarrick. He knows how ter fight.',
+    'We\'re da best. Think diffrent do ya? Come and have a go then, ya runty little wimp!',
+    'All Orks is equal, but some Orks are more equal dan uvvas.',
+    'I am the Arch-fiend, the Despoiler of Worlds, and by my hands shall the false Emperor fall.',
+    'If the path to salvation leads through the halls of purgatory, then so be it.',
+    'Long shall be your suffering. Joyous be your pain.',
+    'Destroy, for the sake of Destruction. Kill, for the sake of Killing.',
+    'Know this! What will come to pass is no longer war! It is endless sacrifice in His name. Blood for the Blood God... let the universe drown in it!',
+    'A man can be convinced to do anything, no matter how abhorrent, with the right motivation.',
+    'Kill! Maim! Burn! Kill! Maim! Burn! Kill! Maim! Burn! Kill! Maim! Burn! Kill! Maim! Burn! Kill! Maim! Burn! Kill! Maim! Burn!',
+    'Question: What has the Emperor ever done for me? Answer: What have you ever done for the Emperor?',
+    'There is only the Emperor, and he is our Shield and Protector.',
+    'Never take a gamble you’re not prepared to lose.',
+    'Life’s so much easier when you’ve got someone to blame.',
+    'Praise the sun that brings the dawn of our final doom.',
+    'You carry the Emperor\'s will as your torch, with it destroy the shadows.',
+    'I haven\'t lost an arm, brother. It\'s right over there.'
+  ];
+  const authors =[
+    'Unknown Nob, Battle of Magna Bonum',
+    'Commonly held Ork view of warfare',
+    'Rotgob, Ork strategist',
+    'Grukk, Ork Boy',
+    'Ghazghkull Mag Uruk Thraka',
+    'Gasgrakh, Goff Nob',
+    'Goff Warboss Bugrat Skumdreg',
+    'Abaddon the Despoiler',
+    'Ahzek Ahriman of the Thousand Sons',
+    'Asteroth, Daemon Prince of Slaanesh',
+    'Unknow',
+    'Crull, Warlord of the World Eaters',
+    'First Chaplain Erebus',
+    'Khârn of the World Eaters',
+    'Training questions, Administration Cant and Dogma',
+    'Statement of fact, Second Book of Chantings',
+    'Abdul Goldberg, Rogue Trader',
+    'Gilbran Quail, Collected Essays',
+    'Unknow',
+    'Verses of Sigismund, Book CIV, Verse I',
+    'Captain Alessio Cortez, Crimson Fists'
   ];
   const answer = possibleAnswers[randomIndex]
   const x = "https://twitter.com/intent/tweet?text="+answer
@@ -69,7 +66,7 @@ function App() {
         <i>{answer}</i>
       </div>
       <div className="z" id="author">
-        - Unknow
+        - {authors[randomIndex]}
       </div>
       <div className="z">
         <a id="tweet-quote" href={x}><button style={{backgroundColor:y} }><Twitter  size={30} /></button></a>
--- src/reducers/randomIndex.js
+++ src/reducers/randomIndex.js
@@ -1,7 +1,7 @@
-const randomReducer = (state = Math.floor(Math.random() * 47), action) => {
+const randomReducer = (state = Math.floor(Math.random() * 21), action) => {
     switch(action.type){
         case 'KEK':
-            state = Math.floor(Math.random() * 47)
+            state = Math.floor(Math.random() * 21)
             return state
         default:
             return state;
